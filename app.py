6eae613fbeddfd8b91f5b9b6140a4d3e1ca02da8
Ritik Raj Kumar <ritikrajkumar007@gmail.com>
2023-04-25 15:38:05 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from tensorflow.keras.preprocessing import image
 # for performing max pooling operation on the spatial dimensions of 2D inputs
 from tensorflow.keras.layers import GlobalMaxPooling2D
 # for use in transfer learning applications
-from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+from tensorflow.keras.applications.resnet import ResNet50, preprocess_input
 # provides unsupervised machine learning methods for nearest neighbor search and clusterin
 from sklearn.neighbors import NearestNeighbors
 
@@ -45,22 +45,27 @@ def save_uploaded_file(uploaded_file):
     except:
         return 0
 
-# Define a function to extract features from an image using the pre-trained ResNet50 model
+# Define a function to extract features from an image using the pre-trained ResNet152 model
 def feature_extraction(img_path, model):
-    # Load the image from the given path and resize it to the input shape of the pre-trained model
-    img = image.load_img(img_path, target_size=(224, 224))
-    # Convert the image to a NumPy array
-    img_array = image.img_to_array(img)
-    # Add a new dimension to the array to create a batch of size 1
-    expanded_img_array = np.expand_dims(img_array, axis=0)
-    # Preprocess the image by applying the same preprocessing steps used during training of the pre-trained model
-    preprocessed_img = preprocess_input(expanded_img_array)
-    # Use the pre-trained model to extract features from the preprocessed image
-    result = model.predict(preprocessed_img).flatten()
-    # Normalize the feature vector to have unit length
-    normalized_result = result / norm(result)
-
-    return normalized_result
+    try:
+        # Load the image from the given path and resize it to the input shape of the pre-trained model
+        img = image.load_img(img_path, target_size=(224, 224))
+        # Convert the image to a NumPy array
+        img_array = image.img_to_array(img)
+        # Add a new dimension to the array to create a batch of size 1
+        expanded_img_array = np.expand_dims(img_array, axis=0)
+        # Preprocess the image by applying the same preprocessing steps used during training of the pre-trained model
+        preprocessed_img = preprocess_input(expanded_img_array)
+        # Use the pre-trained model to extract features from the preprocessed image
+        result = model.predict(preprocessed_img).flatten()
+        # Normalize the feature vector to have unit length
+        normalized_result = result / norm(result)
+        # Returning the result
+        return normalized_result
+    except:
+        # If there is any error while processing the image, print an error message and return None
+        print(f"Error processing file: {img_path}")
+        return None
 
 # function to recommend similar images using k-nearest neighbors
 def recommend(features, feature_list):
@@ -68,10 +73,8 @@ def recommend(features, feature_list):
     neighbors = NearestNeighbors(n_neighbors=5, algorithm='brute', metric='euclidean')
     # fit the NearestNeighbors object to a dataset of feature vectors called feature_list
     neighbors.fit(feature_list)
-
     # find indices of the 5 nearest neighbors of a given feature vector called normalized_result using the NearestNeighbors object
     distances, indices = neighbors.kneighbors([features])
-
     # return the indices of the 5 nearest neighbors
     return indices
 
@@ -83,21 +86,34 @@ def get_pybase64(bin_file):
     return pybase64.b64encode(data).decode()
 
 # function to set the background image of the app
-def set_background(png_file):
+def set_background(png_file, opacity, blur):
     bin_str = get_pybase64(png_file)
-    page_bg_img = '''
+    page_bg_img = f"""
     <style>
-    .stApp {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
+    .stApp {{
+        background-image: url("data:image/png;base64,{bin_str}");
+        opacity: {opacity};
+    }}
+
+    .stApp:before {{
+        content: "";
+        position: absolute;
+        background-size: cover;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        background-image: inherit;
+        filter: blur({blur});
+        z-index: -1;
+    }}
     </style>
-    ''' % bin_str
+    """
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
-
 # set the background image of the app
-set_background('background.png')
+background_image = 'background.png'
+set_background(background_image, 0.9, '2px')
 
 # main app code
 uploaded_file = st.file_uploader("Choose an image")
@@ -111,8 +127,7 @@ if uploaded_file is not None:
         st.image(display_image)
 
         # extract features from the uploaded image using a pre-trained model
-        features = feature_extraction(os.path.join(
-            "uploads", uploaded_file.name), model)
+        features = feature_extraction(os.path.join("uploads", uploaded_file.name), model)
 
         # get similar images based on the extracted features
         indices = recommend(features, feature_list)
